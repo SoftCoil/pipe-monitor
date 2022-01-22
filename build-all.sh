89de97d096733a97ad38851cb420b50d63b6a98f
Cavan Morris <cavanmorris@softcoil.com>
2022-01-22 08:52:46 -0800

    Added .exe extension to Windows binaries.

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+#Build for unixy OSes
 go tool dist list \
   | awk '{split($0,a,"/"); \
   printf "env GOOS=%s GOARCH=%s go build -o bin/%s/%s/pm cmd/pm/main.go\n",a[1], a[2], a[1], a[2]}' \
-  | sh
+  | grep -v "GOOS=windows" | sh
+
+#Build for windows with .exe extension
+go tool dist list \
+  | awk '{split($0,a,"/"); \
+  printf "env GOOS=%s GOARCH=%s go build -o bin/%s/%s/pm.exe cmd/pm/main.go\n",a[1], a[2], a[1], a[2]}' \
+  | grep "GOOS=windows" | sh
